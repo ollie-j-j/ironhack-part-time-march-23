40fe10a3a779212118484ef6e09b3d69f84154fd
LloydC <lloydchambrier@gmail.com>
2023-03-25 09:21:35 +0100

    day 3 add

---
+++ module1/lesson-03/ex-functions.js
@@ -0,0 +1,19 @@
+// Day 3
+
+// Write a function that takes in a parameter "name", tells the user what name was entered and returns it.
+// i.e: "John" ---> "Hi John"
+
+
+// Write a function that takes in a parameter "input" and returns the last character in that string
+// i.e: "Rijksmuseum" ---> "m"
+
+// Write a function that takes in one parameter "length" and prints out that many stars
+// i.e: 3 ---> ***
+
+// Write a function that takes in two parameters "length" and "height" and prints out a rectangle of stars
+// i.e: 3,2 ---> ***
+//               ***
+
+// Write a function that takes in an array of integers and returns the average of the array
+// i.e: [2,5,8] ---> 5
+//      [1,2,3] ---> 2
---
+++ module1/lesson-03/ex-objects.js
@@ -0,0 +1,3 @@
+// Create a fruit object with properties name, color, score, price, quantity
+
+// Create a book object with properties title, author, ratings, hasAward
