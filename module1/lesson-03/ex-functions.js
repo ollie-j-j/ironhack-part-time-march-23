1c511bfd7c669ab3a96667152164323e510ef74e
LloydC <lloydchambrier@gmail.com>
2023-03-25 14:59:31 +0100

    ex-functions code update

--- module1/lesson-03/ex-functions.js
+++ module1/lesson-03/ex-functions.js
@@ -3,13 +3,39 @@
 // Write a function that takes in a parameter "name", tells the user what name was entered and returns it.
 // i.e: "John" ---> "Hi John"
 
+function sayHi(name){
+    return `Hi ${name}`;
+}
+
+console.log(sayHi("John"))
+console.log(sayHi("Julie"))
 
 // Write a function that takes in a parameter "input" and returns the last character in that string
 // i.e: "Rijksmuseum" ---> "m"
 
+// "John" ---> 3 // .length --> 4
+// "Julie" --> 4 // .length --> 5
+function lastCharacter(input){
+    return input[input.length - 1]
+}
+
+console.log(lastCharacter("John"))
+console.log(lastCharacter("Julie"))
+
 // Write a function that takes in one parameter "length" and prints out that many stars
 // i.e: 3 ---> ***
+function numberOfStars(length){
+    return "*".repeat(length);
+}
 
+console.log(numberOfStars(3)) // "***"
+console.log(numberOfStars(5)) // "*****"
+
+function printStars(word){
+    return "*".repeat(word.length);
+}
+
+console.log('printStars', printStars("Johnny"))
 // Write a function that takes in two parameters "length" and "height" and prints out a rectangle of stars
 // i.e: 3,2 ---> ***
 //               ***
