f3e473e46490c95b715842707b5ba1964577fde1
LloydC <lloydchambrier@gmail.com>
2023-04-04 20:47:25 +0200

    Animal class add

--- module1/lesson-07/day-07.js
+++ module1/lesson-07/day-07.js
@@ -1,3 +1,56 @@
+// name, fur, size, colour,
+class Animal {
+    constructor(name, size, colour, fur){
+        this.name = name;
+        this.size = size;
+        this.colour = colour;
+        this.fur = fur;
+
+    }
+
+    feed(){
+        console.log(`${this.name} is hungry`);
+    }
+}
+
+class Pet extends Animal {
+    constructor(name, size, colour, fur, owner){
+        super(name, size, colour, fur);
+        this.owner = owner;
+    }
+
+}
+
+const jellePet = new Pet('rabbit', '15cm', 'white', true, 'Jelle')
+const olliesPet = new Pet('labrador dog', '30cm', 'black', true, 'Ollie')
+const hesamPet = new Pet('german shepherd', '15cm', 'brown', true, 'Hesam')
+const vivianePet = new Pet('frog', '10cm', 'green', false, 'Viviane');
+
+console.log(jellePet)
+
+// jellePet.feed();
+// olliesPet.feed();
+// hesamPet.feed();
+// vivianePet.feed();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Professor { // blueprint for an Object Professor in my system
     constructor(name, researchArea, nobelPrize){
         this.name = name;
@@ -25,11 +78,11 @@ class Professor { // blueprint for an Object Professor in my system
 const einstein = new Professor('Albert einstein', 'Mathematics', true)
 const hawkings = new Professor('Stephen Hawkings', 'Physics', false)
 
-console.log('einstein', einstein)
-console.log('hawkings', hawkings)
+// console.log('einstein', einstein)
+// console.log('hawkings', hawkings)
 
-einstein.doesResearch()
-hawkings.doesResearch()
+// einstein.doesResearch()
+// hawkings.doesResearch()
 
-einstein.checkNobel('1960')
-hawkings.checkNobel('2013')
+// einstein.checkNobel('1960')
+// hawkings.checkNobel('2013')
