cef38fb6a45dc491b4ecaab9ee9129cd1121f7dd
LloydC <lloydchambrier@gmail.com>
2023-04-08 14:51:06 +0200

    day-09 code add

---
+++ module1/lesson-09/js/animation.js
@@ -0,0 +1,10 @@
+document.getElementById("hamburger").addEventListener("click", function(){
+    const navMenu = document.getElementById("navMenu");
+
+    if(navMenu.style.display === ""){
+        navMenu.style.display = "block";
+    }
+   // write the logic to hide and show the navigation menu
+   // Updating the css display property from "none" to "block"
+   // And vice-versa
+})
