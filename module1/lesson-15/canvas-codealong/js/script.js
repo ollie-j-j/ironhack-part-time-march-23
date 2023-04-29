9f42e65b88584b569eff9e8d78fe0621aa3d49e3
LloydC <lloydchambrier@gmail.com>
2023-04-29 15:42:05 +0200

    lesson-15 code updateObstacles add

--- module1/lesson-15/canvas-codealong/js/script.js
+++ module1/lesson-15/canvas-codealong/js/script.js
@@ -1,3 +1,5 @@
+const myObstacles = [];
+
 class Component {
     constructor(width, height, color, x, y) {
       this.width = width;
@@ -20,6 +22,13 @@ class Component {
         this.x += this.speedX;
         this.y += this.speedY;
       }
+
+      jump(){
+        // this.y needs to mimic a jump
+
+        // this.y needs to decrease for a set time period
+        // this.y needs to return back to it's initial position
+      }
 }
 
 function updateGameArea() { // updating our Game
@@ -27,16 +36,42 @@ function updateGameArea() { // updating our Game
     player.newPos(); // update 
     player.update(); // update the player's position
     // update the backGround movement
+    updateObstacles();
 }
+
+function updateObstacles() {
+    for (i = 0; i < myObstacles.length; i++) {
+        myObstacles[i].x += -1; // decreasing the X position of each obstacle by 1
+        myObstacles[i].update(); // showing the new position of the obstacle
+    }
+
+    myGameArea.frames += 1; // 1sec ---> 50 frames 120 frames === 2.4s
+
+    if (myGameArea.frames % 120 === 0) { // if myGameArea.frames === 0 || myGameArea.frames === x * 120
+    // console.log(myGameArea.frames)
+    console.log(myObstacles)
+      let x = myGameArea.canvas.width;
+      let minHeight = 20;
+      let maxHeight = 200;
+      let height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+      let minGap = 50;
+      let maxGap = 200;
+      let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+
+      myObstacles.push(new Component(10, height, 'green', x, 0)); // at the end of the canvas screen, starting from the top
+      myObstacles.push(new Component(10, x - height - gap, 'green', x, height + gap));
+    }
+  }
 
 const myGameArea = {
     canvas: document.createElement('canvas'),
+    frames: 0,
     start: function () {
       this.canvas.width = 480;
       this.canvas.height = 270;
       this.context = this.canvas.getContext('2d');
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.interval = setInterval(updateGameArea, 20); // execute updateGameArea every 20ms
+      this.interval = setInterval(updateGameArea, 20); // execute updateGameArea every 20ms --> updateGameArea gets called 50 times per second
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -51,6 +86,7 @@ const player = new Component(30, 30, 'red', 0, 110); // generate the player
 document.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
       case 38: // up arrow
+      // trigger the jump
         player.speedY -= 1;
         break;
       case 40: // down arrow
