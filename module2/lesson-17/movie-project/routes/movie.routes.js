ea39cc78bbeb91283b6674b103d8f7bcb9a7de6f
LloydC <lloydchambrier@gmail.com>
2023-07-06 22:01:24 +0200

    lesson-17 add

---
+++ module2/lesson-17/movie-project/routes/movie.routes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+
+const Movie = require('../models/Movie.model');
+const fileUploader = require('../config/cloudinary.config');
+ 
+// GET route to display the form to create a new movie
+router.get('/movies/create', (req, res) => res.render('movie-views/movie-create'));
+
+router.post('/movies/create', fileUploader.single('movie-cover-image'), (req, res) => {
+    const { title, description } = req.body;
+    console.log('req.file', req.file)
+   
+    Movie.create({ title, description, imageUrl: req.file.path })
+      .then(newlyCreatedMovieFromDB => {
+        console.log(newlyCreatedMovieFromDB);
+        res.redirect('/movies')
+        // res.render('movie-views/movie-create', {status: 'Movie was created successfully'})
+      })
+      .catch(error => console.log(`Error while creating a new movie: ${error}`));
+  });
+ 
+router.get('/movies', (req, res) => {
+    Movie.find()
+      .then(moviesFromDB => {
+        // console.log(moviesFromDB);
+        res.render('movie-views/movies-list.hbs', { movies: moviesFromDB });
+      })
+      .catch(err => console.log(`Error while getting the movies from the DB: ${err}`));
+  });
+
+  router.get('/movies/:id/edit', (req, res) => {
+    const { id } = req.params;
+   
+    Movie.findById(id)
+      .then(movieToEdit => res.render('movie-views/movie-edit', movieToEdit))
+      .catch(error => console.log(`Error while getting a single movie for edit: ${error}`));
+  });
+
+  router.post('/movies/:id/edit', fileUploader.single('movie-cover-image'), (req, res) => {
+    const { id } = req.params;
+    const { title, description, existingImage } = req.body;
+   
+    let imageUrl;
+    if (req.file) {
+      imageUrl = req.file.path;
+    } else {
+      imageUrl = existingImage;
+    }
+   
+    Movie.findByIdAndUpdate(id, { title, description, imageUrl }, { new: true })
+      .then(() => res.redirect(`/movies`))
+      .catch(error => console.log(`Error while updating a single movie: ${error}`));
+  });
+module.exports = router;
---
+++ module2/lesson-17/movie-project/server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
+const PORT = process.env.PORT || 3000;
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/movie-project";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const databaseName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+  })
+  .then(()=> {
+    app.listen(PORT, () => {
+      console.log(`Server listening on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
